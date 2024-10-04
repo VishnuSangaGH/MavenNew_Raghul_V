574a1c368d248ed5a728cf8a88ae6043423e86e9
irinr <irinr@IRINRAGHUL>
2024-10-04 21:22:58 +0530

    ready to push

---
+++ Maven_2.0/src/test/java/OrganizationTest/ToCreateOrganizationTest.java
@@ -0,0 +1,8 @@
+package OrganizationTest;
+
+public class ToCreateOrganizationTest {
+	public static void main(String[] args) {
+		System.out.println("organization created successfully");
+	}
+
+}
---
+++ Maven_2.0/src/test/java/contactTest/ToCreateContactTest.java
@@ -0,0 +1,8 @@
+package contactTest;
+
+public class ToCreateContactTest {
+	public static void main(String[] args) {
+		System.out.println("Contact created Successfully");
+	}
+
+}
